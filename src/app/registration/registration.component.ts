9f302f42ddabcaa7dbaefc9ad6b75351cd6eccd0
redhat2121 <redhat2121@protonmail.com>
2024-04-16 07:29:22 +0530

    updated reg

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-=======
-import { HttpClient } from '@angular/common/http';
->>>>>>> origin/master
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -16,10 +12,7 @@ export class RegistrationComponent {
   successMessage: string | undefined;
   errorMessage: string | undefined;
   formControls: FormGroup;
-<<<<<<< HEAD
   btnColor = 'rgb(100, 150, 200)';
-=======
->>>>>>> origin/master
 
   constructor(
     private http: HttpClient,
@@ -27,21 +20,14 @@ export class RegistrationComponent {
     private router: Router
   ) {
     this.formControls = this.fb.group({
-<<<<<<< HEAD
       userName: ['', [Validators.required, Validators.pattern('^[a-zA-Z]{1}[a-zA-Z0-9]{2,6}[0-9]?$|^.{4,12}$')]],
       email: ['', [Validators.required, Validators.email]],
       userPassword: ['', [Validators.required, Validators.pattern('^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[@#$%^&+=!])(?=.{4,12}$).*')]],
-=======
-      userName: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      userPassword: ['', Validators.required],
->>>>>>> origin/master
     });
   }
 
   signup() {
     const userData = {
-<<<<<<< HEAD
       userName: this.formControls.value.userName,
       email: this.formControls.value.email,
       userPassword: this.formControls.value.userPassword,
@@ -66,28 +52,5 @@ export class RegistrationComponent {
         this.successMessage = '';
       }
     );
-=======
-      userName: this.formControls.get('userName')?.value,
-      email: this.formControls.get('email')?.value,
-      userPassword: this.formControls.get('userPassword')?.value,
-    };
-
-    this.http.post<any>('http://localhost:3000/users', userData)
-      .subscribe(response => {
-        this.successMessage = 'Registration successful! Redirecting to login...';
-        this.errorMessage = ''; 
-        this.formControls = this.fb.group({
-          userName: ['', Validators.required],
-          email: ['', [Validators.required, Validators.email]],
-          userPassword: ['', Validators.required],
-        });
-        setTimeout(() => {
-          this.router.navigateByUrl('/login'); 
-        }, 2000);
-      }, error => {
-        this.errorMessage = 'Registration failed. Please try again.';
-        this.successMessage = '';
-      });
->>>>>>> origin/master
   }
 }
